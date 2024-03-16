398a6dd391ed64daf4b401072e7825afebee26a2
Monabbir-Ahmmad <monabbir.ahmmad@gmail.com>
2024-03-17 01:47:28 +0600

    Add @react-oauth/google package and update API URLs

--- src/constants/apiUrl.ts
+++ src/constants/apiUrl.ts
@@ -3,6 +3,8 @@ const APIUrl = {
   auth: {
     signin: () => "/auth/login",
     signup: () => "/auth/signup",
+    socialSignin: () => "/auth/social-signin",
+    googleApiSignin: () => "https://www.googleapis.com/oauth2/v3/userinfo",
     signout: () => "/auth/logout",
     refreshToken: () => "/auth/refresh",
     forgotPassword: () => "/auth/forgot-password",
