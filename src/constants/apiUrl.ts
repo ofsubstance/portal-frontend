c4361ef782c719e0cc84cf82589c8aef043d1a7b
Tausif Ahmed <tausifahmed0235@gmail.com>
2025-04-10 03:59:43 +0600

    add ec2 api url

--- src/constants/apiUrl.ts
+++ src/constants/apiUrl.ts
@@ -1,7 +1,7 @@
 import { PlaylistTag } from './enums';
 
 const APIUrl = {
-  base: import.meta.env.VITE_API || 'https://of-substance-backend.onrender.com',
+  base: import.meta.env.VITE_API || 'http://3.144.87.242:5000/api',
   auth: {
     signin: () => '/auth/login',
     signup: () => '/auth/signup',
